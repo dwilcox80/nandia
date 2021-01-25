f85942fbe7c2dc1dd61298326e4111d5a8063610
dwilcox80 <david_wilcox80@hotmail.com>
2021-01-25 08:18:02 -0600

    add styling; root project name change

--- pizza/views.py
+++ pizza/views.py
@@ -79,6 +79,6 @@ def edit_order(request, pk):
             filled_form.save()
             form = filled_form
             note = 'Order has been updated'
-            return render(request, 'pizza/edit_order.html', {'pizzaform': form, 'pizza': pizza,
+            return render(request, 'pizza/edit_order.html', {'form': form, 'pizza': pizza,
                                                              'note': note})
-    return render(request, 'pizza/edit_order.html', {'pizzaform': form, 'pizza': pizza})
+    return render(request, 'pizza/edit_order.html', {'form': form, 'pizza': pizza})
